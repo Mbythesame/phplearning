6769c158375b6423897cd83af69ceb0d0b54ec0b
Mbythesame <mwzheng2016@163.com>
2018-04-10 19:00:48 +0800

    correct
    correct the translation

--- stringtest.php
+++ stringtest.php
@@ -4,7 +4,7 @@ echo htmlentities('<strong>我是吴祁！</strong>').'<br/>'; //转换所有字
 echo htmlspecialchars('<strong>我是吴祁！</strong>') .'<br/>';//转换特殊字符
 echo strip_tags('<strong>我是吴祁！</strong>').'<br/>' //去掉了<strong>
 */
-//echo addslashes('This is \a" Teacher! ');//将单双引号反斜杠前加上个slash即斜杠,用stripslashes可以取消即跳过(strip)斜杠
+//echo addslashes('This is \a" Teacher! ');//将单双引号反斜杠前加上个slash即斜杠,用stripslashes可以删除(strip)斜杠
 //echo str_pad('Salad',15,' may ').'is good enough.';//填充字符串
 //$email = 'yc60.com@gmail.com';
 //$email_array = explode('@',$email);//将字符串内容切割并且转换为数组,切割标志位@.可用implode()或者join()合并
